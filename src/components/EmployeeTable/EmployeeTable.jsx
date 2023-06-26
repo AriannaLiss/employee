019d2fc2814ce2af80e05e5aaef59a21bbfd435e
Lidiia Kalakutska <lidia.kalakutskaya@gmail.com>
2023-06-26 21:22:06 +0200

    Added sorting by any column.

--- src/components/EmployeeTable/EmployeeTable.jsx
+++ src/components/EmployeeTable/EmployeeTable.jsx
@@ -1,18 +1,48 @@
-import { Table, TableContainer } from '@mui/material';
+import { Paper, Table, TableContainer } from '@mui/material';
 import React from 'react';
 import EmployeeTableBody from '../EmployeeTableBody/EmployeeTableBody';
 import EmployeeTableHead from '../EmployeeTableHead/EmployeeTableHead';
 
 const EmployeeTable = ({data}) => {
+    
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('name');
+
+    const handleRequestSort = (e, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    function descendingComparator(a, b, orderBy) {
+        if (b[orderBy] < a[orderBy]) return -1;
+        if (b[orderBy] > a[orderBy]) return 1;
+        return 0;
+    }
+  
+    function getComparator(order, orderBy) {
+        console.log('orderBy', orderBy)
+        return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+    }
+
+    const sortedData = React.useMemo(() => 
+        data.slice().sort( getComparator(order, orderBy) ),
+        [order, orderBy])
+  
 
     return (
-        <TableContainer>
+        <TableContainer component={Paper}>
             <Table
                 sx={{ minWIdth: 750}}
-                size = 'medium'
+                size = 'small'
             >
-                <EmployeeTableHead/>
-                <EmployeeTableBody rows={data}/>
+                <EmployeeTableHead 
+                    order={order}
+                    orderBy={orderBy}
+                    onRequestSort={handleRequestSort}/>
+                <EmployeeTableBody rows={sortedData}/>
             </Table>
             
         </TableContainer>
--- src/components/EmployeeTableHead/EmployeeTableHead.jsx
+++ src/components/EmployeeTableHead/EmployeeTableHead.jsx
@@ -1,20 +1,38 @@
-import { TableCell, TableHead, TableRow } from '@mui/material';
 import React from 'react';
+import { Box, TableCell, TableHead, TableRow, TableSortLabel } from '@mui/material';
+import { visuallyHidden } from '@mui/utils';
 
-const EmployeeTableHead = () => {
-    const headCells = ['Name', 'Job Title', 'Tenure', 'Gender'];
+
+const EmployeeTableHead = ({ order, orderBy, onRequestSort }) => {
+    const headCells = [
+        {id:'name', label:'Name'},
+        {id:'jobTitle', label:'Job Title'},
+        {id:'tenure', label:'Tenure'},
+        {id:'gender', label:'Gender'}
+    ];
+    
     return (
-        <TableHead>
+        <TableHead >
             <TableRow>
                 {headCells.map(headCell => 
                     <TableCell
-                        key={headCell}
+                        key={headCell.id}
                         align='left'
-                        backgroundColor = 'grey'
-                        color ='fff'
-                        // sortDirection={false}
+                        sx={{bgcolor:'grey', color:'white'}}
+                        sortDirection={orderBy === headCell.id ? order : false}
                     >
-                        {headCell}
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={e => onRequestSort(e, headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
                     </TableCell>
                 )}
             </TableRow>
