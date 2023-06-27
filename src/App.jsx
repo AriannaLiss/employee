443e746333d8d44ab611cc18ffa1c1b94cbf7a58
Lidiia Kalakutska <lidia.kalakutskaya@gmail.com>
2023-06-27 15:11:56 +0200

    Added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import './App.css'
 import EmployeeTable from './components/EmployeeTable/EmployeeTable';
+import Loader from './components/UI/Loader/Loader';
 import { get } from './utils/get';
 
 const App = () => {
 
     const [employees, setEmployess] = useState([]);
+    const [isLoading, setLoading] = useState(true)
 
     async function getEmployees(){
         if (employees.length>0) return;
+        setLoading(true)
         setEmployess(await get());
+        setLoading(false)
     }
 
     useEffect(()=>{
         getEmployees();
     })
     return (
-        <div>
+        <>
             <EmployeeTable data={employees}/>
-        </div>
+            {isLoading?<Loader/>:''}
+        </>
     );
 };
 
--- src/components/EmployeeTable/EmployeeTable.jsx
+++ src/components/EmployeeTable/EmployeeTable.jsx
@@ -1,5 +1,5 @@
 import { Paper, Table, TableContainer } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { getComparator } from '../../utils/methods';
 import EmployeeTableBody from '../EmployeeTableBody/EmployeeTableBody';
@@ -7,9 +7,9 @@ import EmployeeTableHead from '../EmployeeTableHead/EmployeeTableHead';
 
 const EmployeeTable = ({data}) => {
     
-    const [order, setOrder] = React.useState('asc');
-    const [orderBy, setOrderBy] = React.useState(null);
-    const [sortedData, setSortedData] = React.useState(data);
+    const [order, setOrder] = useState('asc');
+    const [orderBy, setOrderBy] = useState(null);
+    const [sortedData, setSortedData] = useState(data);
 
     const handleRequestSort = (e, property) => {
         const isAsc = orderBy === property && order === 'asc';
